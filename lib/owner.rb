00389cbd675d0f02b2dcaeb6baec276e0bb47eb5
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-19 19:07:18 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -71,7 +71,7 @@ end
   end
 
 def list_pets
- " I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+ "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
 end
 
 end #ends the class
