41902601ba8a38600ce6792b5bdda3362fc496db
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-19 18:49:13 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,20 +5,24 @@ class Owner
   attr_accessor :name, :pets
   attr_reader :species
 
-
+  def self.count #can keep track of owners that have been created
+     OWNERS.size
+   end
 
   def initialize
     @pets = {fishes: [], cats: [], dogs: []}
-    @owner = owner
+   OWNERS << self # can initialize an owner 
     @species = species
 end
 
-def buy_fish(name)
-  pets[:fishes] << Fish.new(name)
-end
+
 # Associate that new pet instance to the owner
 #by adding it to the appropriate array-value of
 #the @pets hash stored in the pets attr_accessor.
+def buy_fish(name)
+  pets[:fishes] << Fish.new(name)
+end
+
 def buy_cat(name)
   pets[:cats] << Cat.new(name)
 end
