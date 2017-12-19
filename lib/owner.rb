c13f3769e13b722c65899a36adc7e92f884d0f7e
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-19 17:14:01 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,17 @@
 class Owner
   # code goes here
-  attr_accessor :owner :fish :cat :dog 
+  attr_accessor :owner :fish :cat :dog :pets
 
   def initialize
     @pets = {fishes: [], cats: [], dogs: []}
 end
+
+buy_(name)
+@name = name 
+@pets << name 
+# Associate that new pet instance to the owner 
+#by adding it to the appropriate array-value of 
+#the @pets hash stored in the pets attr_accessor.
+end 
+
+end #ends the class 
